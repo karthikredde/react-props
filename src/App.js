39b8ed11df927204069e8f0d313fe607b840f460
saikumar3 <saikumarbonthu9@gmail.com>
2022-11-30 16:52:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <section>
+    <PropsExample name="mini" age="20"/>
+    <PropsExample name="neela" age="20"/>
+      {/*<h1>What was the AI present in Apple Devices</h1>
+      <p>Hello Im Siri Your Voice Assistant</p>
+      <img src="https://cdn.vox-cdn.com/thumbor/_fPdHVaC0IKY1Vn2pHsjdMA0i_U=/0x0:1080x607/1400x1050/filters:focal(540x304:541x305)/cdn.vox-cdn.com/uploads/chorus_asset/file/20046971/eXURD57.png" width="40%" alt="siri"/>
+      <ClassComponent/>*/}
+    </section>
+    )
 }
-
-export default App;
+export default App
